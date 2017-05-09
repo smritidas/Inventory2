37642549b190609be7d8a72c8e4efbf4c58ddd6c
Smriti Das <dassmrit@gmail.com>
2017-05-09 15:39:39 -0400

    Updated code in ItemProvider.

--- app/src/main/java/com/example/android/inventory/data/ItemProvider.java
+++ app/src/main/java/com/example/android/inventory/data/ItemProvider.java
@@ -1,6 +1,5 @@
 package com.example.android.inventory.data;
 
-
 import android.content.ClipData;
 import android.content.ContentProvider;
 import android.content.ContentUris;
@@ -12,40 +11,38 @@ import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
-import java.net.URI;
 
-import static android.icu.text.UnicodeSet.CASE;
-import static com.example.android.inventory.R.string.price;
-import static com.example.android.inventory.R.string.quantity;
+import com.example.android.inventory.R;
 
-//TODO check the import statements
+import static com.example.android.inventory.data.ItemContract.ItemEntry;
 
 public class ItemProvider extends ContentProvider {
 
-    //Tag for the log messages
-    public static final String LOG_TAG = ItemProvider.class.getSimpleName();
-
-    //URI matcher code for the content URI for the entire table
-    public static final int INVENTORY = 100;
-
-    //URI matcher code for the content URI for a single item in the table
-    public static final int INVENTORY_ID = 101;
 
     /**
-     * UriMatcher object to match a content URI to a corresponding code.
-     * The input passed into the constructor represents the code to return for the root URI.
-     * It's common to use NO_MATCH as the input for this case.
+     * URI matcher code for the content URI for the inventory table
      */
+    private static final int INVENTORY = 100;
+
+    /**
+     * URI matcher code for the content URI for single item in the inventory table
+     */
+    private static final int INVENTORY_ID = 101;
+
     private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
-    //This is static and runs the first time anything is called from this class.
     static {
+
         sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEM, INVENTORY);
+
         sUriMatcher.addURI(ItemContract.CONTENT_AUTHORITY, ItemContract.PATH_ITEM + "/#", INVENTORY_ID);
     }
 
-    //Database helper object
+    /**
+     * Database helper object
+     */
     private ItemDBHelper mDBHelper;
+
 
     @Override
     public boolean onCreate() {
@@ -53,53 +50,86 @@ public class ItemProvider extends ContentProvider {
         return true;
     }
 
-
+    @Nullable
     @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    public Cursor query(Uri uri,
+                        String[] projection,
+                        String selection,
+                        String[] selectionArgs,
+                        String sortOrder) {
 
-        SQLiteDatabase database = mDBHelper.getReadableDatabase();
-
-        //hold the results of the query
+        SQLiteDatabase db = mDBHelper.getReadableDatabase();
         Cursor cursor;
 
         int match = sUriMatcher.match(uri);
         switch (match) {
             case INVENTORY:
-
-            cursor = database.query(ItemContract.ItemEntry.TABLE_NAME, projection, selection, selectionArgs,
-                    null, null, sortOrder);
-            break;
+                cursor = db.query(ItemEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
 
             case INVENTORY_ID:
-                selection = ItemContract.ItemEntry._ID + "=?";
-                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                cursor = database.query(ItemContract.ItemEntry.TABLE_NAME, projection, selection, selectionArgs,
-                        null, null, sortOrder);
+
+                selection = ItemEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+
+                cursor = db.query(ItemEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+
                 break;
             default:
                 throw new IllegalArgumentException("Cannot query unknown URI " + uri);
         }
-        //
         cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
         return cursor;
     }
 
+    @Nullable
     @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
-
+    public String getType(Uri uri) {
         final int match = sUriMatcher.match(uri);
         switch (match) {
             case INVENTORY:
-                return insertItem(uri, contentValues);
+                return ItemEntry.CONTENT_LIST_TYPE;
+            case INVENTORY_ID:
+                return ItemEntry.CONTENT_ITEM_TYPE;
             default:
-                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
         }
+
     }
 
-    private Uri insertItem(Uri uri, ContentValues values){
-        String name = values.getAsString(ItemContract.ItemEntry.COLUMN_ITEM_NAME);
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case INVENTORY:
+                return insertProduct(uri, values);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri.toString());
+        }
+
+
+    }
+
+    public Uri insertProduct(Uri uri, ContentValues values) {
+
+        // Check that the name is not null
+        String name = values.getAsString(ItemEntry.COLUMN_ITEM_NAME);
         if (name == null) {
-            throw new IllegalArgumentException("Item requires a name");
+            throw new IllegalArgumentException("Product requires a name");
         }
 
         Double price = values.getAsDouble(ItemContract.ItemEntry.COLUMN_PRICE);
@@ -115,125 +145,80 @@ public class ItemProvider extends ContentProvider {
         // Get writeable database
         SQLiteDatabase database = mDBHelper.getWritableDatabase();
 
-        // Insert the new pet with the given values
-        long id = database.insert(ItemContract.ItemEntry.TABLE_NAME, null, values);
+
+        // Insert the new product with the given values
+        long id = database.insert(ItemEntry.TABLE_NAME, null, values);
         // If the ID is -1, then the insertion failed. Log an error and return null.
         if (id == -1) {
             return null;
         }
 
-        // Notify all listeners that the data has changed for the pet content URI
+        // Notify all listeners that the data has changed for the inventory content URI
         getContext().getContentResolver().notifyChange(uri, null);
+
 
         // Return the new URI with the ID (of the newly inserted row) appended at the end
         return ContentUris.withAppendedId(uri, id);
     }
 
-    @Override
-    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
-
-        final int match = sUriMatcher.match(uri);
-        switch (match) {
-            case INVENTORY:
-                return updateItem(uri, contentValues, selection, selectionArgs);
-            case INVENTORY_ID:
-                // For the ITEMS_ID code, extract out the ID from the URI,
-                // so we know which row to update. Selection will be "_id=?" and selection
-                // arguments will be a String array containing the actual ID.
-                selection = ItemContract.ItemEntry._ID + "=?";
-                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                return updateItem(uri, contentValues, selection, selectionArgs);
-            default:
-                throw new IllegalArgumentException("Update is not supported for " + uri);
-        }
-    }
-
-    private int updateItem(Uri uri, ContentValues values, String selection, String[] selectionArgs){
-        if (values.containsKey(ItemContract.ItemEntry.COLUMN_ITEM_NAME)){
-            String name = values.getAsString(ItemContract.ItemEntry.COLUMN_ITEM_NAME);
-            if (name == null) {
-                throw new IllegalArgumentException("Item requires a name");
-            }
-        }
-
-        if (values.containsKey(ItemContract.ItemEntry.COLUMN_PRICE)) {
-            Double price = values.getAsDouble(ItemContract.ItemEntry.COLUMN_PRICE);
-            if (price == null || price < 0.00 ) {
-                throw new IllegalArgumentException("Pet requires a valid price");
-            }
-        }
-
-        if (values.containsKey(ItemContract.ItemEntry.COLUMN_QUANTITY)) {
-            Integer amount = values.getAsInteger(ItemContract.ItemEntry.COLUMN_QUANTITY);
-            if (amount == null || amount < 0 ) {
-                throw new IllegalArgumentException("Not a valid quantity");
-            }
-        }
-        if (values.size() == 0) {
-            return 0;
-        }
-
-        SQLiteDatabase database = mDBHelper.getWritableDatabase();
-
-        // Update the database and get the number of rows affected
-        int rowsUpdated = database.update(ItemContract.ItemEntry.TABLE_NAME, values, selection, selectionArgs);
-
-        // This is called if more than 1 row gets updated
-        if (rowsUpdated != 0) {
-            getContext().getContentResolver().notifyChange(uri, null);
-        }
-
-        // Return the number of rows updated
-        return rowsUpdated;
-
-    }
-
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-
+        // Get writable database
         SQLiteDatabase database = mDBHelper.getWritableDatabase();
-
-        // Track the number of rows that were deleted
+        int match = sUriMatcher.match(uri);
         int rowsDeleted;
-
-        final int match = sUriMatcher.match(uri);
         switch (match) {
             case INVENTORY:
-                // Delete all rows that match the selection and selection args
-                rowsDeleted = database.delete(ItemContract.ItemEntry.TABLE_NAME, selection, selectionArgs);
+                rowsDeleted = database.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
                 break;
             case INVENTORY_ID:
-                // Delete a single row given by the ID in the URI
-                selection = ItemContract.ItemEntry._ID + "=?";
-                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
-                rowsDeleted = database.delete(ItemContract.ItemEntry.TABLE_NAME, selection, selectionArgs);
+                selection = ItemEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                rowsDeleted = database.delete(ItemEntry.TABLE_NAME, selection, selectionArgs);
                 break;
             default:
                 throw new IllegalArgumentException("Deletion is not supported for " + uri);
         }
-
-        // If 1 or more rows were deleted, then notify all listeners that the data at the
-        // given URI has changed
         if (rowsDeleted != 0) {
             getContext().getContentResolver().notifyChange(uri, null);
         }
 
-        // Return the number of rows deleted
         return rowsDeleted;
-
     }
 
     @Override
-    public String getType(Uri uri) {
-        final int match = sUriMatcher.match(uri);
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        // Get writable database
+        SQLiteDatabase database = mDBHelper.getWritableDatabase();
+        int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        if (contentValues == null) {
+            throw new IllegalArgumentException("Cannot update empty values");
+        }
+
+        //todo add Validations of fields that are updated
+
         switch (match) {
             case INVENTORY:
-                return ItemContract.ItemEntry.CONTENT_LIST_TYPE;
+                rowsUpdated = database.update(ItemEntry.TABLE_NAME,
+                        contentValues,
+                        selection,
+                        selectionArgs);
+                break;
             case INVENTORY_ID:
-                return ItemContract.ItemEntry.CONTENT_ITEM_TYPE;
+                rowsUpdated = database.update(ItemEntry.TABLE_NAME,
+                        contentValues,
+                        ItemEntry._ID + " = ?",
+                        new String[]{String.valueOf(ContentUris.parseId(uri))});
+                break;
             default:
-                throw new IllegalStateException("Unknown URI " + uri + " with match " + match);
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
+
+        return rowsUpdated;
     }
+
 }
+
