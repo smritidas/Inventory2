8aaf35fe8f19d75c3763d626b19beab795f7fe56
Smriti Das <dassmrit@gmail.com>
2017-05-15 19:12:47 -0400

    Updated code in ItemProvider.

--- app/src/main/java/com/example/android/inventory/data/ItemDBHelper.java
+++ app/src/main/java/com/example/android/inventory/data/ItemDBHelper.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.android.inventory.data.ItemContract.ItemEntry;
+
 //Database helper for the app
 
 public class ItemDBHelper extends SQLiteOpenHelper{
@@ -27,12 +29,13 @@ public class ItemDBHelper extends SQLiteOpenHelper{
 
     //called when database is called for the first time
     @Override
-    public void onCreate(SQLiteDatabase db){
-        String SQL_CREATE_TABLE = "CREATE TABLE " + ItemContract.ItemEntry.TABLE_NAME + " (" +
-                ItemContract.ItemEntry._ID + " INTEGER PRIMARY KEY, " +
-                ItemContract.ItemEntry.COLUMN_ITEM_NAME + " TEXT NOT NULL, " +
-                ItemContract.ItemEntry.COLUMN_PRICE + " REAL NOT NULL DEFAULT 0, " +
-              ItemContract.ItemEntry.COLUMN_QUANTITY + " INTEGER NOT NULL);";
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_TABLE = "CREATE TABLE " + ItemEntry.TABLE_NAME + " ("
+                + ItemEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ItemEntry.COLUMN_ITEM_NAME + " TEXT NOT NULL, "
+                + ItemEntry.COLUMN_PRICE + " REAL NOT NULL DEFAULT 0.0, "
+                + ItemEntry.COLUMN_QUANTITY + " INTEGER NOT NULL DEFAULT 0, "
+                + ");";
 
         db.execSQL(SQL_CREATE_TABLE);
     }
